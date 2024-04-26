c8d4e411505663bcaa71bb78492099385e48c5df
Pedro <pedrohrb77@gmail.com>
2024-04-26 15:52:34 -0300

    chore: fix which key

--- lua/settings/plugins/which-key.lua
+++ lua/settings/plugins/which-key.lua
@@ -1,16 +1,13 @@
 return {
 	"folke/which-key.nvim",
-	config = function()
+	event = "VeryLazy",
+	init = function()
 		vim.o.timeout = true
-		vim.o.timeoutlen = 500
-
-		local wkey = require("which-key")
-		wkey.setup({
-			opts = {
-				-- your configuration comes here
-				-- or leave it empty to use the default settings
-				-- refer to the configuration section below
-			},
-		})
+		vim.o.timeoutlen = 300
 	end,
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+	},
 }
