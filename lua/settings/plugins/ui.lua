c670c13bfaf24948f792937b0992e60ae86f22fa
Pedro <pedrohrb77@gmail.com>
2024-04-26 10:53:50 -0300

    chore: change theme style and make transparent

--- lua/settings/plugins/ui.lua
+++ lua/settings/plugins/ui.lua
@@ -1,16 +1,24 @@
 return {
-  -- the colorscheme should be available when starting Neovim
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight]])
-    end,
-  },
+	-- the colorscheme should be available when starting Neovim
+	{
+		"folke/tokyonight.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
+		config = function()
+			local tkn = require("tokyonight")
+			tkn.setup({
+				style = "night",
+				transparent = true,
+				styles = {
+					sidebars = "transparent",
+				},
+			})
+			-- load the colorscheme here
+			vim.cmd([[colorscheme tokyonight]])
+		end,
+	},
 
-  -- I have a separate config.mappings file where I require which-key.
-  -- With lazy the plugin will be automatically loaded when it is required somewhere
-  { "folke/which-key.nvim", lazy = true }
+	-- I have a separate config.mappings file where I require which-key.
+	-- With lazy the plugin will be automatically loaded when it is required somewhere
+	{ "folke/which-key.nvim", lazy = true },
 }
