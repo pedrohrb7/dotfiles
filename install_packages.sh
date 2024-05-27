bf9c051d56598529b6fb01d4e62ccd085717d36b
Pedro <pedrohrb77@gmail.com>
2024-05-27 20:44:31 -0300

    chore: add install script

---
+++ install_packages.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# validate if its running with root
+if ["$(id -u)" -ne 0 ]; then
+    echo "Must run as root"
+    exit 1
+fi
+
+# validate if yay or paru is installed
+if [! command -v yay &> /dev/null]; then
+    echo "yay not installed..."
+else 
+    echo "installing yay..."   
+    git clone https://aur.archlinux.org/yay.git
+    cd yay || exit
+    makepkg -si --noconfirm
+    cd ..
+    rm -rf yay
+    echo "yay installed..."
+fi
+
+# Lista de pacotes a serem instalados com pacman
+pacman_packages=(
+  "neovim"
+  "git"
+  "htop"
+  "curl"
+  "wget"
+  "neofetch"
+  "gcc"
+  "make"
+  "fd"
+  "ripgrep"
+  "bat"
+  "eza"
+  "kitty"
+  "zsh"
+  "xclip"
+  "fzf"
+  "polybar"
+  "rofi"
+  "conky"
+  "archlinux-keyring"
+  "picom"
+)
+
+# Lista de pacotes a serem instalados com yay
+aur_packages=(
+  "fzf-git"
+  "arcolinux-logout"
+)
+
+# install pacman packages
+install_pacman_packages() {
+  echo "Sync and Update system..."
+  pacman -Syu --noconfirm
+
+  echo "Installing PACMAN packages..."
+  for package in "${pacman_packages[@]}"; do
+    pacman -S --noconfirm "$package"
+  done
+}
+
+# install aur packages
+install_aur_packages() {
+  echo "Installing AUR packages..."
+  for package in "${aur_packages[@]}"; do
+    yay -S --noconfirm "$package"
+  done
+}
+
+install_pacman_packages()
+install_aur_packages()
+
+echo "All packages installed with sucess... Logout and login again!"
