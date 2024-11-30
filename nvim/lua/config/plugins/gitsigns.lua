fe80c2b4b19652824b3a6d1f80928f09e0401a48
Pedro <pedrohrb77@gmail.com>
2024-11-30 19:13:20 -0300

    feat(lib): add new plugins

--- nvim/lua/config/plugins/gitsigns.lua
+++ nvim/lua/config/plugins/gitsigns.lua
@@ -16,7 +16,7 @@ return {
           follow_files = true,
         },
         signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-        numhl = true,  -- Toggle with `:Gitsigns toggle_numhl`
+        numhl = true, -- Toggle with `:Gitsigns toggle_numhl`
         linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
         word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
         current_line_blame = true,
