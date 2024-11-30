ae60aeba689a5db3dc86c56b9e50cbfcd6df1838
Pedro <pedrohrb77@gmail.com>
2024-11-30 19:23:17 -0300

    chore(wk): wip setup menu
    Still validating how to setup keymaps, how to load from keymaps.lua

--- nvim/lua/config/plugins/which-key.lua
+++ nvim/lua/config/plugins/which-key.lua
@@ -1,19 +1,71 @@
 return {
-	"folke/which-key.nvim",
-	event = "VeryLazy",
-	opts = {
-		---@type false | "classic" | "modern" | "helix"
-		preset = "modern",
-	},
-	keys = {
-		{
-			"<leader>",
-			function()
-        local wk = require("which-key") 
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  lazy = true,
+  opts = {
+    ---@type false | "classic" | "modern" | "helix"
+    preset = "modern",
+    icons = {
+      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
+      separator = "➜", -- symbol used between a key and it's label
+      group = "+", -- symbol prepended to a group
+      ellipsis = "…",
+      -- set to false to disable all mapping icons,
+      -- both those explicitly added in a mapping
+      -- and those from rules
+      mappings = true,
+      --- See `lua/which-key/icons.lua` for more details
+      --- Set to `false` to disable keymap icons from rules
+      ---@type wk.IconRule[]|false
+      rules = {},
+      -- use the highlights from mini.icons
+      -- When `false`, it will use `WhichKeyIcon` instead
+      colors = true,
+      -- used by key format
+      keys = {
+        Up = " ",
+        Down = " ",
+        Left = " ",
+        Right = " ",
+        C = "󰘴 ",
+        M = "󰘵 ",
+        D = "󰘳 ",
+        S = "󰘶 ",
+        CR = "󰌑 ",
+        Esc = "󱊷 ",
+        ScrollWheelDown = "󱕐 ",
+        ScrollWheelUp = "󱕑 ",
+        NL = "󰌑 ",
+        BS = "󰁮",
+        Space = "󱁐 ",
+        Tab = "󰌒 ",
+        F1 = "󱊫",
+        F2 = "󱊬",
+        F3 = "󱊭",
+        F4 = "󱊮",
+        F5 = "󱊯",
+        F6 = "󱊰",
+        F7 = "󱊱",
+        F8 = "󱊲",
+        F9 = "󱊳",
+        F10 = "󱊴",
+        F11 = "󱊵",
+        F12 = "󱊶",
+      },
+    },
+    show_help = true, -- show a help message in the command line for using WhichKey
+    show_keys = true, -- show the currently pressed key and its label as a message in the command line
+  },
+  confi = function()
+    local wk = require("which-key")
 
-				wk.show({ global = true })
-			end,
-			desc = "Buffer Local Keymaps (which-key)",
-		},
-	},
+    wk.registers("config.core.keymaps", {
+      mode = "n", -- NORMAL mode
+      prefix = "<leader>",
+      buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
+      silent = true, -- use `silent` when creating keymaps
+      noremap = true, -- use `noremap` when creating keymaps
+      nowait = true, -- use `nowait` when creating keymaps
+    })
+  end,
 }
