6ae6ee7b7149f199217610c9bb27cee12d1a43a8
Pedro <pedrohrb77@gmail.com>
2024-12-02 19:34:31 -0300

    chore: make ufo ignore neotree

--- nvim/lua/config/plugins/nvim_ufo.lua
+++ nvim/lua/config/plugins/nvim_ufo.lua
@@ -21,5 +21,13 @@ return {
         return { 'lsp', 'indent' }
       end,
     })
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = { 'nvcheatsheet', 'neo-tree' },
+      callback = function()
+        require('ufo').detach()
+        vim.opt_local.foldenable = false
+      end,
+    })
   end,
 }
