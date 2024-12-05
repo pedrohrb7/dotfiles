61c4463bbc23f39425afc489ad15ecc45ff6b34b
Pedro <pedrohrb77@gmail.com>
2024-12-04 22:17:27 -0300

    chore: change wk theme

--- nvim/lua/config/plugins/which-key.lua
+++ nvim/lua/config/plugins/which-key.lua
@@ -1,15 +1,15 @@
 return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
+  'folke/which-key.nvim',
+  event = 'VeryLazy',
   lazy = true,
   opts = {
     ---@type false | "classic" | "modern" | "helix"
-    preset = "modern",
+    preset = 'helix',
     icons = {
-      breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-      separator = "➜", -- symbol used between a key and it's label
-      group = "+", -- symbol prepended to a group
-      ellipsis = "…",
+      breadcrumb = '»', -- symbol used in the command line area that shows your active key combo
+      separator = '➜', -- symbol used between a key and it's label
+      group = '+', -- symbol prepended to a group
+      ellipsis = '…',
       -- set to false to disable all mapping icons,
       -- both those explicitly added in a mapping
       -- and those from rules
@@ -23,45 +23,46 @@ return {
       colors = true,
       -- used by key format
       keys = {
-        Up = " ",
-        Down = " ",
-        Left = " ",
-        Right = " ",
-        C = "󰘴 ",
-        M = "󰘵 ",
-        D = "󰘳 ",
-        S = "󰘶 ",
-        CR = "󰌑 ",
-        Esc = "󱊷 ",
-        ScrollWheelDown = "󱕐 ",
-        ScrollWheelUp = "󱕑 ",
-        NL = "󰌑 ",
-        BS = "󰁮",
-        Space = "󱁐 ",
-        Tab = "󰌒 ",
-        F1 = "󱊫",
-        F2 = "󱊬",
-        F3 = "󱊭",
-        F4 = "󱊮",
-        F5 = "󱊯",
-        F6 = "󱊰",
-        F7 = "󱊱",
-        F8 = "󱊲",
-        F9 = "󱊳",
-        F10 = "󱊴",
-        F11 = "󱊵",
-        F12 = "󱊶",
+        Up = ' ',
+        Down = ' ',
+        Left = ' ',
+        Right = ' ',
+        C = '󰘴 ',
+        M = '󰘵 ',
+        D = '󰘳 ',
+        S = '󰘶 ',
+        CR = '󰌑 ',
+        Esc = '󱊷 ',
+        ScrollWheelDown = '󱕐 ',
+        ScrollWheelUp = '󱕑 ',
+        NL = '󰌑 ',
+        BS = '󰁮',
+        Space = '󱁐 ',
+        Tab = '󰌒 ',
+        F1 = '󱊫',
+        F2 = '󱊬',
+        F3 = '󱊭',
+        F4 = '󱊮',
+        F5 = '󱊯',
+        F6 = '󱊰',
+        F7 = '󱊱',
+        F8 = '󱊲',
+        F9 = '󱊳',
+        F10 = '󱊴',
+        F11 = '󱊵',
+        F12 = '󱊶',
       },
     },
     show_help = true, -- show a help message in the command line for using WhichKey
     show_keys = true, -- show the currently pressed key and its label as a message in the command line
   },
   confi = function()
-    local wk = require("which-key")
+    local wk = require('which-key')
 
-    wk.registers("config.core.keymaps", {
-      mode = "n", -- NORMAL mode
-      prefix = "<leader>",
+    wk.setup(opts)
+    wk.registers('config.core.keymaps', {
+      mode = 'n', -- NORMAL mode
+      prefix = '<leader>',
       buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
       silent = true, -- use `silent` when creating keymaps
       noremap = true, -- use `noremap` when creating keymaps
