87269ce6e85bcf121ff0fd99e62ae7f628ddc383
Pedro <pedrohrb77@gmail.com>
2025-08-16 17:29:00 -0300

    chore: update imgs | hide gpu widget
    FIX:TODO(gpu_widget)
    its crashing

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -34,7 +34,7 @@ local brightness_widget = require("configs.widgets.brightness-widgets.brightness
 local battery_widget = require("configs.widgets.battery")
 local theme = require("themes.default.theme")
 local color = require("configs.color")
-local gpu_widget = require("configs.widgets.gpu")
+-- local gpu_widget = require("configs.widgets.gpu")
 local mem_widget = require("configs.widgets.mem")
 
 local modkey = "Mod4"
@@ -294,7 +294,7 @@ awful.screen.connect_for_each_screen(function(s)
 			rounded_widget({ widget = mykeyboardlayout }),
 			rounded_widget({ widget = volume_widget }),
 			rounded_widget({ widget = cpu_widget() }),
-			rounded_widget({ widget = gpu_widget }),
+			-- rounded_widget({ widget = gpu_widget }),
 			rounded_widget({
 				widget = mem_widget({
 					timeout = 5,
