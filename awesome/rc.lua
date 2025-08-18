279cf18b928d035441718843a24fc49ea39c9caf
Pedro <pedrohrb77@gmail.com>
2025-08-17 22:45:09 -0300

    chore: remove tasklist layout

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -207,48 +207,6 @@ awful.screen.connect_for_each_screen(function(s)
 		screen = s,
 		filter = awful.widget.tasklist.filter.currenttags,
 		buttons = tasklist_buttons,
-		layout = {
-			spacing_widget = {
-				{
-					forced_width = 8,
-					forced_height = 24,
-					thickness = 1,
-					color = "#777777",
-					widget = wibox.widget.separator,
-				},
-				valign = "center",
-				halign = "center",
-				widget = wibox.container.place,
-			},
-			spacing = 2,
-			layout = wibox.layout.fixed.horizontal,
-		},
-		-- widget_template = {
-		-- 	{
-		-- 		wibox.widget.base.make_widget(),
-		-- 		forced_height = 2,
-		-- 		id = "background_role",
-		-- 		widget = wibox.container.background,
-		-- 	},
-		-- 	{
-		-- 		{
-		-- 			id = "clienticon",
-		-- 			widget = awful.widget.clienticon,
-		-- 		},
-		-- 		margins = 2,
-		-- 		widget = wibox.container.margin,
-		-- 	},
-		-- 	nil,
-		-- 	create_callback = function(self, c, index, objects) --luacheck: no unused args
-		-- 		self:get_children_by_id("clienticon")[1].client = c
-		-- 	end,
-		-- 	layout = wibox.layout.align.vertical,
-		-- },
-		-- style = {
-		-- 	shape_border_width = 1,
-		-- 	shape_border_color = "#ff77f7",
-		-- 	shape = gears.shape.rounded,
-		-- },
 	})
 
 	-- Create the wibox
