0379dbeb6e9aa519fbee599a3d351ff69f6932af
Pedro <pedrohrb77@gmail.com>
2026-01-23 22:56:40 -0300

    remove unsed cofigs

--- polybar/launch.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-killall polybar
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
-
-CONFIG_DIR=$HOME/dotfiles-config/window-manager/polybar/custom/config.ini
-polybar -c $CONFIG_DIR &
